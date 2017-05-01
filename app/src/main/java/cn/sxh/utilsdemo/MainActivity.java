48a7a4acfb03579f6fb6b2f35a69a48bfb309140
1215167396@qq.com <1215167396@qq.com>
2017-05-01 20:31:07 +0800

    1.待完善

---
+++ app/src/main/java/cn/sxh/utilsdemo/MainActivity.java
@@ -0,0 +1,47 @@
+package cn.sxh.utilsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    protected BaseAdapter mAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.listview);
+        mAdapter = new ArrayAdapter<>(this, R.layout.list_item,
+                R.id.tv_item, getResources().getStringArray(R.array.util_list));
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                findID_Item(i);
+            }
+        });
+    }
+
+    private void findID_Item(int i) {
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+}
